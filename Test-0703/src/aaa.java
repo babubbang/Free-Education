06180798d60527e8868a27257ef739cf27b98c25
PC <PC@DESKTOP-B8DETJS>
2023-07-03 14:49:37 +0900

    김정인 사실 한국 좋아해 한(나라)

--- Test-0703/src/aaa.java
+++ Test-0703/src/aaa.java
@@ -3,11 +3,8 @@ public class aaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("±èÁ¤ÀÎÀº ´ÏÈ¥Áø");
-=======
 		System.out.println("±èÁ¤ÀÎÀº ÇÑ±¹ÀÌ Á¤¸» ½È¾î¿ä");
->>>>>>> branch 'main' of https://github.com/babubbang/Free-Education
 	}
 
 }
