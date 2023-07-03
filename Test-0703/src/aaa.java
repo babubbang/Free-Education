2be6cb3a7dd01ef02af9b5a48d11bb064edada7d
양수민 <@DESKTOP-U4JS6I4>
2023-07-03 14:59:14 +0900

    뿌엥

--- Test-0703/src/aaa.java
+++ Test-0703/src/aaa.java
@@ -5,7 +5,7 @@ public class aaa {
 		// TODO Auto-generated method stub
 		System.out.println("김정인은 니혼진");
 		System.out.println("김정인은 한국이 정말 싫어요");
-		System.out.println("정기 MT 추진좀,,,,");
+		System.out.println("정기 MT 추진좀,,,, 뿌엥");
 	}
 
 }
