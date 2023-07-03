ca26ad3852f31e1a1abd6b7578667c5c3e463550
양수민 <@DESKTOP-U4JS6I4>
2023-07-03 14:43:24 +0900

    2

--- Test-0703/src/aaa.java
+++ Test-0703/src/aaa.java
@@ -3,7 +3,7 @@ public class aaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("김정인 한국이 좋아요");
+		System.out.println("김정인은 한국이 싫어요");
 	}
 
 }
