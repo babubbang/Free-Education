51948b01f1b9fb3078fb12295ccaba45af099d33
kji99 <kji99@DESKTOP-L4NA797.mshome.net>
2023-07-03 14:37:34 +0900

    한국 변경

--- Test-0703/src/aaa.java
+++ Test-0703/src/aaa.java
@@ -3,7 +3,7 @@ public class aaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("김정인 중국몽 옹호");
+		System.out.println("김정인 한국이 좋아요");
 	}
 
 }
