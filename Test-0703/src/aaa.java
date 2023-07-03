58dd1579d15d1b08e66e0ddd49ffd9cc4f3f3685
S <S@Song>
2023-07-03 14:47:06 +0900

    일본

--- Test-0703/src/aaa.java
+++ Test-0703/src/aaa.java
@@ -3,7 +3,7 @@ public class aaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("김정인은 한국사람이 아니에요");
+		System.out.println("김정인은 니혼진");
 	}
 
 }
