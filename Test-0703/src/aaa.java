c231f31302d27ec4d64fb09b35c40b333fced647
choipillmuk <pm2431@naver.com>
2023-07-03 14:46:30 +0900

    학편 한국 싫어해...

--- Test-0703/src/aaa.java
+++ Test-0703/src/aaa.java
@@ -3,7 +3,7 @@ public class aaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("김정인은 한국이 싫어요");
+		System.out.println("김정인은 한국이 정말 싫어요");
 	}
 
 }
