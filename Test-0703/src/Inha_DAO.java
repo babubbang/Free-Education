ab119a17d06412e16db297d99566f5529f75b2ab
PC <PC@DESKTOP-B8DETJS>
2023-07-26 18:40:34 +0900

    d

--- Test-0703/src/Inha_DAO.java
+++ Test-0703/src/Inha_DAO.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 
 public class Inha_DAO {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver"); 
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/inha", "root", "1234");
