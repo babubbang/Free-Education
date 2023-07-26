ccc836edd00ea10119f7da8881d3cc539ea08d0b
PC <PC@DESKTOP-B8DETJS>
2023-07-26 17:33:08 +0900

    db접속

--- Test-0703/src/Inha_DAO.java
+++ Test-0703/src/Inha_DAO.java
@@ -1,24 +1,24 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Inha_DAO {
 
 	public static void main(String[] args) {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://122.43.183.107:3306/inha", "root", "1234");
-			Statement stmt = conn.createStatement();
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/inha", "root", "1234");
+			System.out.println("mysql 연결 성공");
+				
 			
-			stmt.executeUpdate("insert into tb1 (id, name) values ('kim', 'jungin')");
-			
-			
-			stmt.close();
 			conn.close();
 			
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (ClassNotFoundException error) {
+			System.out.println("mysql driver 미설치 또는 드라이버 이름 오류");
+		} catch (SQLException error) {
+			System.out.println("DB 접속오류");
 		}
 	}
 
